48f7255996f676e1454e5ad2280aa0738809725d
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-12-30 10:55:45 +0200

    Added a simple way to scale SVG.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use sdl2::{
     event::Event,
     image::InitFlag,
     keyboard::Keycode,
+    rwops::RWops,
     sys::{
         image::IMG_LoadSVG_RW, SDL_CreateRenderer, SDL_CreateTextureFromSurface, SDL_Rect,
-        SDL_RenderClear, SDL_RenderCopy, SDL_RenderPresent,
+        SDL_RenderClear, SDL_RenderCopy, SDL_RenderPresent, SDL_Renderer, SDL_Texture,
     },
 };
 
@@ -16,13 +17,9 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
     let renderer = unsafe { SDL_CreateRenderer(window.raw(), 0, 0) };
 
-    let svg = "<svg height='400' width='400'><circle cx='200' cy='200' r='160' stroke='white' stroke-width='4' fill='black'/></svg>";
-    let data = sdl2::rwops::RWops::from_bytes(svg.as_bytes()).unwrap();
-
-    // The following function call will actually also rasterize the vector image.
-    let surface = unsafe { IMG_LoadSVG_RW(data.raw()) };
-
-    let texture = unsafe { SDL_CreateTextureFromSurface(renderer, surface) };
+    let mut width = 400;
+    let mut height = 400;
+    let mut texture = svg_circle(renderer, width, height);
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -32,24 +29,47 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Plus),
+                    ..
+                } => {
+                    height += 10;
+                    width += 10;
+                    texture = svg_circle(renderer, height, width);
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Minus),
+                    ..
+                } => {
+                    height -= 10;
+                    width -= 10;
+                    texture = svg_circle(renderer, height, width);
+                }
                 _ => {}
             }
         }
         let source_rect = SDL_Rect {
             x: 0,
             y: 0,
-            w: 400,
-            h: 400,
+            w: width,
+            h: height,
         };
         let texture_rect = SDL_Rect {
-            x: 100,
-            y: 100,
-            w: 400,
-            h: 400,
+            x: 0,
+            y: 0,
+            w: width,
+            h: height,
         };
         unsafe { SDL_RenderClear(renderer) };
         unsafe { SDL_RenderCopy(renderer, texture, &source_rect, &texture_rect) };
         unsafe { SDL_RenderPresent(renderer) };
         ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
+}
+
+fn svg_circle(renderer: *mut SDL_Renderer, h: i32, w: i32) -> *mut SDL_Texture {
+    let svg = format!("<svg height='{}' width='{}' viewBox='0 0 400 400'><circle cx='200' cy='200' r='160' stroke='white' stroke-width='4' fill='black'/></svg>", h, w);
+    let data = RWops::from_bytes(svg.as_bytes()).unwrap();
+    let surface = unsafe { IMG_LoadSVG_RW(data.raw()) };
+    unsafe { SDL_CreateTextureFromSurface(renderer, surface) }
 }
