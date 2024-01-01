7f625b562cd6ace1886f34ff32d7181b6613af5b
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2024-01-01 08:54:14 +0200

    Added a support for single and double quotes for width and height pattern.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::OnceLock;
+use std::{fs, sync::OnceLock};
 
 use regex::Regex;
 use sdl2::{
@@ -74,17 +74,19 @@ fn build_sprite(texture_creator: &TextureCreator<WindowContext>, h: u32, w: u32)
     // Lazily initialize static regexes for width and height.
     static W_REGEX: OnceLock<Regex> = OnceLock::new();
     static H_REGEX: OnceLock<Regex> = OnceLock::new();
-    let w_regex = W_REGEX.get_or_init(|| Regex::new(r"(<svg[^>]*width=')(?:[0-9]+)").unwrap());
-    let h_regex = H_REGEX.get_or_init(|| Regex::new(r"(<svg[^>]*height=')(?:[0-9]+)").unwrap());
+    let w_regex = W_REGEX.get_or_init(|| Regex::new(r#"(<svg[^>]*width=["'])(?:[0-9]+)"#).unwrap());
+    let h_regex =
+        H_REGEX.get_or_init(|| Regex::new(r#"(<svg[^>]*height=["'])(?:[0-9]+)"#).unwrap());
 
     // TODO load this string from a file...
-    let svg = "<svg height='400' width='400' viewBox='0 0 400 400'><circle cx='200' cy='200' r='160' stroke='white' stroke-width='4' fill='black'/></svg>";
+    let svg = fs::read_to_string("test.svg").unwrap();
+    // let svg = "<svg height='400' width='400' viewBox='0 0 400 400'><circle cx='200' cy='200' r='160' stroke='white' stroke-width='4' fill='black'/></svg>";
 
     let replace_w = String::from("${1}") + w.to_string().as_str();
     let replace_h = String::from("${1}") + w.to_string().as_str();
 
     // TODO Check how to handle these 'Cow' values.
-    let mut modified_svg = w_regex.replacen(svg, 1, replace_w).to_string();
+    let mut modified_svg = w_regex.replacen(&svg, 1, replace_w).to_string();
     modified_svg = h_regex.replacen(&modified_svg, 1, replace_h).to_string();
 
     Sprite {
