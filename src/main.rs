a370412637fdf327aaa6ff1ef70bd80bb4296db8
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2024-01-01 08:42:13 +0200

    Lazily initialize width and height regex only once.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::OnceLock;
+
 use regex::Regex;
 use sdl2::{
     event::Event,
@@ -69,19 +71,21 @@ struct Sprite<'a> {
 }
 
 fn build_sprite(texture_creator: &TextureCreator<WindowContext>, h: u32, w: u32) -> Sprite {
+    // Lazily initialize regex for width and height.
+    static W_REGEX: OnceLock<Regex> = OnceLock::new();
+    static H_REGEX: OnceLock<Regex> = OnceLock::new();
+    let w_regex = W_REGEX.get_or_init(|| Regex::new(r"(<svg[^>]*width=')(?:[0-9]+)").unwrap());
+    let h_regex = H_REGEX.get_or_init(|| Regex::new(r"(<svg[^>]*height=')(?:[0-9]+)").unwrap());
+
     // TODO load this string from a file...
     let svg = "<svg height='400' width='400' viewBox='0 0 400 400'><circle cx='200' cy='200' r='160' stroke='white' stroke-width='4' fill='black'/></svg>";
-
-    // TODO handle errors from these patterns and check whether these could be static? ... also fix to match only to svg element.
-    let w_pattern = Regex::new(r"(<svg[^>]*width=')(?:[0-9]+)").unwrap(); // TODO error handling
-    let h_pattern = Regex::new(r"(<svg[^>]*height=')(?:[0-9]+)").unwrap(); // TODO error handling
 
     let replace_w = String::from("${1}") + w.to_string().as_str();
     let replace_h = String::from("${1}") + w.to_string().as_str();
 
     // TODO Check how to handle these 'Cow' values.
-    let mut modified_svg = w_pattern.replacen(svg, 1, replace_w).to_string();
-    modified_svg = h_pattern.replacen(&modified_svg, 1, replace_h).to_string();
+    let mut modified_svg = w_regex.replacen(svg, 1, replace_w).to_string();
+    modified_svg = h_regex.replacen(&modified_svg, 1, replace_h).to_string();
 
     Sprite {
         texture: texture_creator
